4dd95e22e30798471e460dbfdc5c657787fb69ce
ferdy <ferdy@none>
2013-12-08 12:35:38 +0100

    add logging statements

--- src/java/mypackage/HelloworldServlet.java
+++ src/java/mypackage/HelloworldServlet.java
@@ -6,15 +6,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.eclipsesource.json.JsonObject;
 
 public class HelloworldServlet extends HttpServlet {
 	private static final long serialVersionUID = -7759593256585062849L;
+	private static final Logger LOG = LoggerFactory.getLogger(HelloworldServlet.class);
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		LOG.info("serving request");
 		if ("json".equals(req.getParameter("format"))) {
+			LOG.info("special format for request {}", req);
 			resp.setContentType("application/json");
 			String json = new JsonObject().add("msg", "Hello!").toString();
 			resp.getWriter().print(json);
